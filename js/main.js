f123fd2f541a2116e632f3a47c71ed2c7e1afc4a
Xavier Belanche <xbelanch@protonmail.com>
2021-05-12 22:39:27 +0200

    well, this commit is embarrassed...

--- js/main.js
+++ js/main.js
@@ -1,43 +1,67 @@
-var input = document.getElementById("input");
-input.addEventListener("keydown", function (e) {
-    if (e.keyCode === 13) {
-        authenticate(e.target.value);
+let username = document.getElementById("username");
+let password = document.getElementById("pass");
+
+password.addEventListener('keydown', (e) => {
+    if (e.keyCode == 13) { // user press intro
+        // Check if username field is empty
+        if (username.value.length == 0) {
+            console.log('Username is empty!');
+        } else {
+            console.log(
+                'Authenticate username: ' +
+                    username.value
+                    + ' and password: ' +
+                    password.value);
+
+            if (password.value !== 'hola') {
+                username.value = '';
+                username.focus();
+                password.value = '';
+            }
+        }
     }
 });
 
-window.authentication_complete = function() {
-    if (lightdm.is_authenticated) {
-            lightdm.login(lightdm.authentication_user, null);
-    } else {
-        input.value = "";
-        input.placeholder = "user";
-        input.type = "text";
-        input.disabled = false;
-        input.focus();
-        input.select();
-    }
-}
+// var input = document.getElementById("input");
+// input.addEventListener("keydown", function (e) {
+//     if (e.keyCode === 13) {
+//         authenticate(e.target.value);
+//     }
+// });
 
-function authenticate(input_text) {
-    if(!lightdm.in_authentication || !lightdm.authentication_user) {
-        lightdm.authenticate(input_text);
-        input.value = "";
-        input.type = "password";
-        input.placeholder = "password";
-        input.disabled = false;
-    } else {
-        input.disabled = true;
-        lightdm.respond(input_text);
-    }
-}
+// window.authentication_complete = function() {
+//     if (lightdm.is_authenticated) {
+//             lightdm.login(lightdm.authentication_user, null);
+//     } else {
+//         input.value = "";
+//         input.placeholder = "user";
+//         input.type = "text";
+//         input.disabled = false;
+//         input.focus();
+//         input.select();
+//     }
+// }
+
+// function authenticate(input_text) {
+//     if(!lightdm.in_authentication || !lightdm.authentication_user) {
+//         lightdm.authenticate(input_text);
+//         input.value = "";
+//         input.type = "password";
+//         input.placeholder = "password";
+//         input.disabled = false;
+//     } else {
+//         input.disabled = true;
+//         lightdm.respond(input_text);
+//     }
+// }
 
 // --- Booting up
 window.addEventListener('load', () => {
     // new VaporwaveTheme();
-    input.focus();
-    input.select();
-    input.value = lightdm.select_user_hint;
-    if(input.value) {
-      authenticate(input.value);
-    }
+    // input.focus();
+    // input.select();
+    // input.value = lightdm.select_user_hint;
+    // if(input.value) {
+    //   authenticate(input.value);
+    // }
 });
