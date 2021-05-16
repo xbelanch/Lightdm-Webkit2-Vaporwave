5a44327842cacf849f661e8cb703edbdd96d623a
Xavier Belanche <xbelanch@protonmail.com>
2021-05-16 23:06:00 +0200

    Unfinished working username and password prompt

--- js/main.js
+++ js/main.js
@@ -1,67 +1,112 @@
-let username = document.getElementById("username");
-let password = document.getElementById("pass");
+function clear_messages() {
+  messages = document.getElementById("messages");
+  messages.innerHTML = "";
+  messages.style.visibility = "hidden";
+}
 
-password.addEventListener('keydown', (e) => {
-    if (e.keyCode == 13) { // user press intro
-        // Check if username field is empty
-        if (username.value.length == 0) {
-            console.log('Username is empty!');
-        } else {
-            console.log(
-                'Authenticate username: ' +
-                    username.value
-                    + ' and password: ' +
-                    password.value);
-
-            if (password.value !== 'hola') {
-                username.value = '';
-                username.focus();
-                password.value = '';
-            }
-        }
+function start_authentication() {
+    // clear previous alert or warning messages
+    clear_messages();
+    // start with null userid, have pam prompt for userid.
+    lightdm.authenticate();
+    // display dialogue box
+    // you can deactivate later if this visual goodie
+    // doesnt fit your needs
+    // setTimeout(function() {
+    //     // fade in input fields
+    //     document.getElementById('dialogue-box')
+    //         .setAttribute('class', 'active');
+    //     // set hostname
+    //     // document.getElementById('hostname').innerHTML = lightdm.hostname;
+    // }, 250);
+}
+// This function is called by LightDM when authentication has completed.
+function authentication_complete() {
+    if (lightdm.is_authenticated) {
+        lightdm.login(lightdm.authentication_user, "i3");
     }
-});
+}
 
-// var input = document.getElementById("input");
-// input.addEventListener("keydown", function (e) {
-//     if (e.keyCode === 13) {
-//         authenticate(e.target.value);
-//     }
-// });
+// Callbacks
+function attemptLogin(event) {
+    // Prevent Default Form Behavior (Try To Submit To Location)
+    event.preventDefault();
+    entry = document.getElementById("password");
 
-// window.authentication_complete = function() {
-//     if (lightdm.is_authenticated) {
-//             lightdm.login(lightdm.authentication_user, null);
-//     } else {
-//         input.value = "";
-//         input.placeholder = "user";
-//         input.type = "text";
-//         input.disabled = false;
-//         input.focus();
-//         input.select();
-//     }
-// }
+    // Disable All Password Input Forms
+    // disablePasswordForms(true);
+    let password = entry.value;
+    lightdm.respond(password);
+}
 
-// function authenticate(input_text) {
-//     if(!lightdm.in_authentication || !lightdm.authentication_user) {
-//         lightdm.authenticate(input_text);
-//         input.value = "";
-//         input.type = "password";
-//         input.placeholder = "password";
-//         input.disabled = false;
-//     } else {
-//         input.disabled = true;
-//         lightdm.respond(input_text);
-//     }
-// }
+function setActiveUser(event) {
+
+    // Get Information For The Selected User Listing
+    let userListing = event.currentTarget;
+    let userName = lightdm.users[userListing.getAttribute("data-user-index")].name;
+    console.log(userName);
+
+    // fill input with username
+    userListing.value = userName;
+
+    // if selected user is already activeUsername, do nothing
+    if (userName == activeUserName)
+        return;
+
+    // Update Status To Reflect Selected User
+    pendingAuthentication = true;
+    activeUserName = userName;
+    // Focus Password Box
+    userPasswordInput.focus();
+
+    // Begin Authentication
+    lightdm.authenticate(activeUserName);
+}
+
+
+let debugLightDM = {
+    "authenticate" : function(activeUserName) {
+        console.log("lightdm.authenticate:", activeUserName);
+    },
+    "respond" : function(inputText) {
+        console.log("lightdm.respond:", inputText);
+    },
+    "shutdown" : function() {
+        console.log("lightdm.shutdown");
+    },
+    "reboot" : function() {
+        console.log("lightdm.reboot");
+    },
+    "users" : [
+        { name: "rotter" },
+        { name: "lyud" } ,
+        { name: "tomachito" }
+    ],
+    "hostname" : "arch"
+}
 
 // --- Booting up
+let promptForm = document.getElementById("prompt");
+let loginForm = promptForm.querySelector(".password-form");
+let userClickBox = document.getElementById("username");
+let userPasswordInput = document.getElementById("password");
+let activeUserName = null;
+let pendingAuthentication = false;
+
+window.lightdm = window.lightdm || debugLightDM;
 window.addEventListener('load', () => {
-    // new VaporwaveTheme();
-    // input.focus();
-    // input.select();
-    // input.value = lightdm.select_user_hint;
-    // if(input.value) {
-    //   authenticate(input.value);
-    // }
+    start_authentication();
+
+    // add data-user-index
+    // need for user list context
+    userClickBox.setAttribute("data-user-index", 0);
+    // Add Login Form Listener
+    // userClickBox.addEventListener("click", setActiveUser);
+    userClickBox.addEventListener("keydown", function(event) {
+        if (event.keyCode == 9) {
+            event.preventDefault();
+            setActiveUser(event);
+        }
+    })
+    loginForm.addEventListener("submit", attemptLogin);
 });
